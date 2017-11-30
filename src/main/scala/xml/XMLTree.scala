8bb4da839d2de2298758c3b6d0f21f5506b6fc40
robertbutacu <robert.butacu@yahoo.com>
2017-11-30 16:50:55 +0200

    implemented Element() methods

--- src/main/scala/xml/XMLTree.scala
+++ src/main/scala/xml/XMLTree.scala
@@ -2,24 +2,45 @@ package xml
 
 
 case class Element(name: String,
-                   attributes: List[Attribute],
-                   childElements: List[Element],
-                   value: Option[String]) extends XML {
+                   attributes: Set[Attribute],
+                   elements: Set[Element],
+                   value: Option[Value]) extends XML {
   override def toXml: String = ???
 
-  def add(attr: Attribute): Element = ???
+  def add(attr: Attribute): Element =
+    Element(name, attributes + attr, elements, value)
 
-  def add(attrs: List[Attribute]): Element = ???
+  // used DummyImplicit because of type erasure raising an error => Set[Element] fundamentally gets reduced to Set[Obj]
+  def add(attrs: Set[Attribute])(implicit d: DummyImplicit): Element =
+    Element(name, attributes ++ attrs, elements, value)
 
-  def add(child: Element): Element = ???
+  def add(child: Element): Element =
+    Element(name, attributes, elements + child, value)
 
-  def remove(attr: Attribute): Element = ???
+  def add(children: Set[Element]): Element =
+    Element(name, attributes, elements ++ children, value)
 
-  def remove(child: Element): Element = ???
+  def remove(attr: Attribute): Element =
+    Element(name, attributes - attr, elements, value)
 
-  def edit(old: Element, updated: Element): Element = ???
+  def remove(attrs: Set[Attribute]): Element =
+    Element(name, attributes -- attrs, elements, value)
 
-  def edit(newValue: Value): Element = ???
+  def remove(child: Element): Element =
+    Element(name, attributes, elements - child, value)
+
+  // used DummyImplicit because of type erasure raising an error => Set[Element] fundamentally gets reduced to Set[Obj]
+  def remove(children: Set[Element])(implicit d: DummyImplicit): Element =
+    Element(name, attributes, elements -- children, value)
+
+  def edit(old: Element, updated: Element): Element =
+    Element(name, attributes, elements(old) = updated, value)
+
+  def edit(old: Attribute, updated: Attribute): Element =
+    Element(name, attributes(old) = updated, elements, value)
+
+  def edit(newValue: Option[Value]): Element =
+    Element(name, attributes, elements, newValue)
 }
 
 
