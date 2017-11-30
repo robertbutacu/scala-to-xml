2c368180bd7299a2e7d5c93e8a43e7027436aba0
robertbutacu <robert.butacu@yahoo.com>
2017-11-30 12:33:43 +0200

    basic scaffolding

---
+++ src/main/scala/xml/Xml.scala
@@ -0,0 +1,32 @@
+package xml
+
+trait XML{
+  def toXml: String
+}
+
+case class Element(name: String,
+                   attributes: List[Attribute],
+                   childElements: List[Element],
+                   value: Option[String]) extends XML {
+  override def toXml: String = ???
+
+  def addAttribute(attr: Attribute): Element = ???
+
+  def removeAttribute(attr: Attribute): Element = ???
+
+  def addChild(child: Element): Element = ???
+
+  def removeChild(child: Element): Element = ???
+
+  def editChild(old: Element, updated: Element): Element = ???
+
+  def editValue(newValue: Value): Element = ???
+}
+
+case class Attribute(name: String, value: Attribute) extends XML {
+  override def toXml: String = s" $name = \"$value\" "
+}
+
+case class Value(value: String) extends XML {
+  override def toXml: String = s"$value"
+}
