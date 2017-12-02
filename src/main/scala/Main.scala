15a848ae5f7dc390f87c9b3f59acc397549ea2a5
robertbutacu <robert.butacu@yahoo.com>
2017-12-02 10:00:06 +0200

    Writer() implemented

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,10 @@
+import xml.io.Writer
 import xml.{Attribute, Element, Value}
 
 object Main extends App{
-  println(Element("people",
+  println(Writer.save(Element("people",
     elements = Set(Element("person",
       elements = Set(Element("PassionAttr", Set(Attribute("weight", "15")), Set.empty, Some(Value("guitar"))),
         Element("AgeAttr", Set(Attribute("weight", "20")), Set.empty, Some(Value("25")))))),
-    value = None).toXml(0))
+    value = None), "asdfasdfa\\out.xml"))
 }
--- src/main/scala/xml/io/Writer.scala
+++ src/main/scala/xml/io/Writer.scala
@@ -1,7 +1,19 @@
 package xml.io
 
+import java.io.{BufferedWriter, File, FileWriter, PrintWriter}
+
 import xml.Element
 
+import scala.util.Try
+
 object Writer {
-  def save(root: Element, path: String, filename: String): Unit = ???
+  def save(root: Element, filename: String): Try[Unit] = {
+    val rootSerialized = root.toXml()
+    Try {
+      val output = new BufferedWriter(new FileWriter(new File(filename)))
+
+      output.write(rootSerialized)
+      output.close()
+    }
+  }
 }
